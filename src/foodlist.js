db62129ea480421e31817e0e5eb5f49251906465
mrizqputra <rizkysapp@gmail.com>
2022-12-01 14:42:02 +0700

    repair foodlist

--- src/foodlist.js
+++ src/foodlist.js
@@ -252,29 +252,29 @@ function Foodlist() {
           return (
             <>
               <div className="col-md-12 col-lg-6 p-3 mb-3">
-                <div className="card_foodlist">
-                  <div className="card-body">
+                <div className="card_foodlist shadow">
+                  <div className="card-body px-3">
                     <div className="row mb-3">
                       <div className="mb-3 mt-4 col-sm-12 mb-sm-3 col-md-4 col-lg-4 text-center">
                         <img
-                          className="foodlist_img img-fluid"
+                          className="shadow foodlist_img img-fluid"
                           src={item.imageUrl}
                           // style={{ height: "12rem", width: "12rem" }}
                           alt="foodlist-img"
                         />
                       </div>
-                      <div className=" mt-4 col-sm-12 col-md-8 col-lg-8">
-                        <div className="row">
+                      <div className="shadow bg-light rounded py-2 mt-4 col-sm-12 col-md-8 col-lg-8">
+                        <div className="bg-body rounded shadow py-2 px-3 card-text h4 fw-bold">
+                          <span className="orange">{item.name}</span>
+                        </div>
+                        {/* <div className="row">
                           <div className="col-1 col-sm-1 col-md-1 col-lg-1">
                             <div className="h6">ID:</div>
                           </div>
                           <div className="col-11 col-sm-11 col-md-11 col-lg-10">
                             <div className="h6">{item.id}</div>
                           </div>
-                        </div>
-                        <div className="card-text h4 fw-bold">
-                          <span className="orange">{item.name}</span>
-                        </div>
+                        </div> */}
                         <div className="row">
                           <div className="col-3 col-sm-3 col-md-3 col-lg-4">
                             <div className="card-text h6">Description:</div>
@@ -297,56 +297,59 @@ function Foodlist() {
                         </div>
                       </div>
                     </div>
-                    <div className="row">
-                      <div className="col-6 mt-2">Rating: {item.rating}</div>
-                      <div className="col-6">
+                    <div className="row bg-light rounded shadow mb-3">
+                      <div className="col-6">Rating: {item.rating}</div>
+                      <div className="col-4">
+                        <div className="h6">Total likes: {item.totalLikes}</div>
+                      </div>
+                    </div>
+                    <div className="row mb-4 text-center">
+                      <div className="col-3">
                         <button
-                          className="btn"
+                          className="shadow btn btn-light button_submit"
+                          style={{ color: `${item.isLike ? "green" : ""}` }}
                           onClick={() => handleLike(item.id, item.isLike)}
                           type="button"
                         >
-                          {item.totalLikes}
+                          Like!&nbsp;
                           <svg
                             xmlns="http://www.w3.org/2000/svg"
-                            width="16"
-                            height="16"
+                            width="20"
+                            height="20"
                             fill="currentColor"
-                            class="bi bi-heart ms-1"
-                            style={{ color: `${item.isLike ? "red" : ""}` }}
-                            viewBox="0 0 16 16"
+                            style={{ color: `${item.isLike ? "green" : ""}` }}
+                            class="bi bi-hand-thumbs-up"
+                            // viewBox="0 0 16 16"
                           >
-                            <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01L8 2.748zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143c.06.055.119.112.176.171a3.12 3.12 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15z" />
+                            <path d="M8.864.046C7.908-.193 7.02.53 6.956 1.466c-.072 1.051-.23 2.016-.428 2.59-.125.36-.479 1.013-1.04 1.639-.557.623-1.282 1.178-2.131 1.41C2.685 7.288 2 7.87 2 8.72v4.001c0 .845.682 1.464 1.448 1.545 1.07.114 1.564.415 2.068.723l.048.03c.272.165.578.348.97.484.397.136.861.217 1.466.217h3.5c.937 0 1.599-.477 1.934-1.064a1.86 1.86 0 0 0 .254-.912c0-.152-.023-.312-.077-.464.201-.263.38-.578.488-.901.11-.33.172-.762.004-1.149.069-.13.12-.269.159-.403.077-.27.113-.568.113-.857 0-.288-.036-.585-.113-.856a2.144 2.144 0 0 0-.138-.362 1.9 1.9 0 0 0 .234-1.734c-.206-.592-.682-1.1-1.2-1.272-.847-.282-1.803-.276-2.516-.211a9.84 9.84 0 0 0-.443.05 9.365 9.365 0 0 0-.062-4.509A1.38 1.38 0 0 0 9.125.111L8.864.046zM11.5 14.721H8c-.51 0-.863-.069-1.14-.164-.281-.097-.506-.228-.776-.393l-.04-.024c-.555-.339-1.198-.731-2.49-.868-.333-.036-.554-.29-.554-.55V8.72c0-.254.226-.543.62-.65 1.095-.3 1.977-.996 2.614-1.708.635-.71 1.064-1.475 1.238-1.978.243-.7.407-1.768.482-2.85.025-.362.36-.594.667-.518l.262.066c.16.04.258.143.288.255a8.34 8.34 0 0 1-.145 4.725.5.5 0 0 0 .595.644l.003-.001.014-.003.058-.014a8.908 8.908 0 0 1 1.036-.157c.663-.06 1.457-.054 2.11.164.175.058.45.3.57.65.107.308.087.67-.266 1.022l-.353.353.353.354c.043.043.105.141.154.315.048.167.075.37.075.581 0 .212-.027.414-.075.582-.05.174-.111.272-.154.315l-.353.353.353.354c.047.047.109.177.005.488a2.224 2.224 0 0 1-.505.805l-.353.353.353.354c.006.005.041.05.041.17a.866.866 0 0 1-.121.416c-.165.288-.503.56-1.066.56z" />
                           </svg>
-                          {/* </p> */}
                         </button>
                       </div>
-                    </div>
-                    <div className="row mb-4">
-                      <div className="col-4">
+                      <div className="col-3">
                         <button
                           data-bs-toggle="modal"
                           data-bs-target={`#reviewModal-${item.id}`}
                           onClick={() => handleReview(item.id)}
-                          className="btn btn-success button_submit"
+                          className="shadow btn btn-success button_submit"
                         >
                           review
                         </button>
                       </div>
-                      <div className="col-4">
+                      <div className="col-3">
                         <button
                           type="button"
                           onClick={() => handleDelete(item.id)}
-                          className="btn btn-danger button_submit"
+                          className="shadow btn btn-danger button_submit"
                         >
                           Delete
                         </button>
                       </div>
-                      <div className="col-4">
+                      <div className="col-3">
                         <button
                           data-bs-toggle="modal"
                           data-bs-target={`#editFoodModal-${item.id}`}
                           onClick={() => handleEdit(item.id)}
-                          className="btn btn-warning button_submit"
+                          className="shadow btn btn-warning button_submit"
                         >
                           edit
                         </button>
@@ -359,15 +362,13 @@ function Foodlist() {
                 class="modal fade"
                 id={`editFoodModal-${item.id}`}
                 tabindex="-1"
-                // aria-labelledby="exampleModalLabel"
-                // aria-hidden="true"
               >
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">
-                        do you want to edit data : {item.id}
-                      </h1>
+                      <div class="modal-title fs-5 h1" id="exampleModalLabel">
+                        Want to edit @food ID : {item.id}
+                      </div>
                       <button
                         type="button"
                         class="btn-close"
@@ -376,26 +377,28 @@ function Foodlist() {
                       ></button>
                     </div>
                     <div class="modal-body">
-                      <h3 className="input_label">Latest Food Data</h3>
-                      <h5 className="input_label">name: {item.name}</h5>
-                      <h5 className="input_label">
-                        description: {item.description}
-                      </h5>
-                      <h5 className="input_label">
-                        ingredients: {item.ingredients}
-                      </h5>
-                      <img
-                        src={item.imageUrl}
-                        style={{ height: "12rem", width: "12rem" }}
-                        alt="food list img"
-                      />
-                      <div className="input_label">
-                        ========================================
+                      <div className="bg-light rounded shadow py-3 mb-3">
+                        <div className="h4 input_label">Latest Food Data</div>
+                        <div className="h5 input_label">name: {item.name}</div>
+                        <div className="h6 input_label">
+                          description: {item.description}
+                        </div>
+                        <div className="h6 input_label">
+                          ingredients: {item.ingredients.join(", ")}
+                        </div>
+                        <img
+                          src={item.imageUrl}
+                          alt="food list img"
+                          className="img-fluid input_label foodlist_img"
+                        />
                       </div>
                       <form
-                        className="row g-3"
+                        className="row g-1 bg-danger bg-opacity-25 rounded shadow py-3 mt-4 mb-3"
                         onSubmit={(e) => handleSubmit(e, item.id)}
                       >
+                        <div className="h5 fw-bold text-center">
+                          FORM TO EDIT FOOD DATA
+                        </div>
                         <label
                           for="inputName"
                           className="form-label input_label"
@@ -447,11 +450,11 @@ function Foodlist() {
                               >
                                 Ingredients
                               </label>
-                              <div className="container-fluid">
+                              <div className="container mb-3 ">
                                 <input
                                   onBlur={formik.handleBlur}
                                   type="text"
-                                  className="input-group mb-3 input_box"
+                                  className="input-group input_box mb-3"
                                   id="ingredients"
                                   value={ingredient}
                                   onChange={(event) =>
@@ -462,22 +465,29 @@ function Foodlist() {
                                   }
                                 />
                                 <button
-                                  className="btn btn-success button_submitModal"
+                                  className="btn btn-success button_submitModal me-1"
                                   onClick={() => handleAddEditIngredients()}
                                   type="button"
                                 >
                                   Add
                                 </button>
-                                <button
-                                  className="btn btn-danger button_submitModal"
-                                  style={{ marginLeft: "0" }}
-                                  onClick={() =>
-                                    handleRemoveEditIngredients(index)
-                                  }
-                                  type="button"
-                                >
-                                  Delete
-                                </button>
+                                {/* {
+                                  (ingredients.push(() => {
+                                    return ( */}
+                                      <button
+                                        className="btn btn-danger button_submitModal"
+                                        id="removeBtn"
+                                        style={{ marginLeft: "0" }}
+                                        onClick={() =>
+                                          handleRemoveEditIngredients(index)
+                                        }
+                                        type="button"
+                                      >
+                                        Delete
+                                      </button>
+                                    {/* )
+                                  }))
+                                } */}
                               </div>
                               {formik.touched.ingredients &&
                               formik.errors.ingredients ? (
@@ -508,9 +518,10 @@ function Foodlist() {
                 <div class="modal-dialog">
                   <div class="modal-content">
                     <div class="modal-header">
-                      <h1 class="modal-title fs-5" id="exampleModalLabel">
-                        do you want to review : {item.id}
-                      </h1>
+                      <div class="modal-title fs-5 h1" id="exampleModalLabel">
+                        Want to review @food ID:{" "}
+                        <span className="orange">{item.id}</span>
+                      </div>
                       <button
                         type="button"
                         class="btn-close"
@@ -519,43 +530,53 @@ function Foodlist() {
                       ></button>
                     </div>
                     <div class="modal-body">
-                      <h3 className="input_label">Latest Food Data</h3>
-                      <h5 className="input_label">name: {item.name}</h5>
-                      <h5 className="input_label">rating: {item.rating}</h5>
-                      <img
-                        src={item.imageUrl}
-                        style={{ height: "12rem", width: "12rem" }}
-                        alt="food list img"
-                        className="input_label"
-                      />
+                      <div className="bg-light rounded shadow py-3 mb-3">
+                        <div className="h6 input_label">name: {item.name}</div>
+                        <div className="h6 input_label">
+                          rating: {item.rating}
+                        </div>
+                        <img
+                          src={item.imageUrl}
+                          // style={{ height: "12rem", width: "12rem" }}
+                          alt="food list img"
+                          className="img-fluid input_label foodlist_img"
+                        />
+                      </div>
                       {reviewList.map((item) => {
                         console.log(item);
                         return (
-                          <div className="container">
+                          <div className="bg-light rounded shadow py-3 mb-3">
                             <div className="row input_label">
-                              <div className="col-md-3">
+                              <div className="col-2">
                                 {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
                                 <img
-                                  className="img-fluid"
+                                  className="img-fluid review_img"
                                   src={item.user.profilePictureUrl}
                                   alt="reviewer"
                                 />
-                                <div className="h6">{item.user.name}</div>
                               </div>
-                              <div className="col-md-9">
-                                <div className="h4">{item.review}</div>
+                              <div className="col-8 bg-success bg-opacity-10 rounded shadow py-3 mb-3">
+                                <div className="h4">
+                                  <span className="orange">
+                                    {item.user.name}
+                                  </span>
+                                </div>
+                                <div className="h6">{item.review}</div>
                               </div>
                             </div>
                           </div>
                         );
                       })}
-                      <div className="input_label">
-                        ========================================
-                      </div>
                       <form
-                        className="row g-3"
+                        className="row g-1 bg-info bg-opacity-10 rounded shadow py-3 mt-4 mb-3"
                         onSubmit={(e) => handleReviewSubmit(e, item.id)}
                       >
+                        <label
+                          for="inputName"
+                          className="h5 fw-bold text-center"
+                        >
+                          FORM FOR GIVE RATING AND REVIEW
+                        </label>
                         <label
                           for="inputName"
                           className="form-label input_label"
@@ -578,7 +599,7 @@ function Foodlist() {
                           multiple={false}
                           className="form-select input_box mx-auto"
                         >
-                          <option selected>Open this select menu</option>
+                          <option selected>Open this rating value</option>
                           <option value="1">1</option>
                           <option value="2">2</option>
                           <option value="3">3</option>
